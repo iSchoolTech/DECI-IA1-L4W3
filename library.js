d06a4f77d2a214a65644a113d2458c44069c784d
Abdallah Gaber <abdallahgaber441@gmail.com>
2024-10-08 12:15:51 +0300

    Create library.js

---
+++ library.js
@@ -0,0 +1,66 @@
+// Task: Create a Library class and manage a collection of books
+
+
+
+class Library {
+  constructor() {
+    this.books = []; // Array to hold all book objects
+  }
+
+  addBook(book) {
+    this.books.push(book); // Add a new book to the library
+  }
+
+  removeBook(title) {
+    const index = this.books.findIndex(book => book.title === title); // Find the book by title
+    if (index !== -1) {
+      const removedBook = this.books.splice(index, 1); // Remove the book from the array
+    }
+  }
+
+  searchBook(title) {
+    const book = this.books.find(book => book.title === title); // Search for a book by title
+    if (book) {
+      console.log(`Found: ${book.getDetails()}`); // If found, show details
+    }
+  }
+
+  listBooks() {
+    if (this.books.length === 0) {
+      console.log('No books in the library.'); // Inform if no books are present
+    } else {
+      this.books.forEach(book => console.log(book.getDetails())); // List all books
+    }
+  }
+}
+
+const myLibrary = new Library();
+
+// Create a new book object using Book class
+const book1 = new Book();
+book1.setTitle('The Great Gatsby');
+book1.setAuthor('F. Scott Fitzgerald');
+book1.setYear(1925);
+
+// Create a new book object using Book class
+const book2 = new Book();
+book2.setTitle('1984');
+book2.setAuthor('George Orwell');
+book2.setYear(1949);
+
+// Add books to the library
+myLibrary.addBook(book1);
+myLibrary.addBook(book2);
+
+// List all books
+myLibrary.listBooks();
+
+// Search for a book
+myLibrary.searchBook('1984');
+
+// Remove a book
+myLibrary.removeBook('The Great Gatsby');
+
+// List all books again
+myLibrary.listBooks();
+
